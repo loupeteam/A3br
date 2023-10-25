80012886e8d3c29875739750e76ce356d85708fc
agrayzel <ari@loupe.team>
2023-10-25 11:26:56 -0400

    Add missed headers

--- src/Ar/A3BR/base64.h
+++ src/Ar/A3BR/base64.h
@@ -1,6 +1,10 @@
-//********************************************************************************
-// Author:    Andrew Musser
-// Created:   June 22, 2022
-//********************************************************************************
+/*
+ * File: base64.h
+ * Copyright (c) 2023 Loupe
+ * https://loupe.team
+ * 
+ * This file is part of A3BR, licensed under the MIT License.
+ * 
+ */
 
 int Base64encode(char *encoded, const char *string, int len);
